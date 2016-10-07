1ba11dc837e319504145d7d22b57bae7416ae106
IuliiaPopova <popovasqa@gmail.com>
2016-10-06 20:43:01 -0700

    sws

---
+++ ClassYulia/ClassYulia/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassYulia
+{
+    class Doctor
+    {
+        public string surname;
+        public string name;
+        public string middleName;
+
+        public string profession;
+        public int cabinetNumber;
+        public int patientNumber;
+
+        public void PrintBio()
+        {
+            Console.WriteLine("Doctor {0} {1} {2} has profession \"{3}\", make appoitment in the cabinet number {4}. He has {5} patients.", surname, name, middleName, profession, cabinetNumber, patientNumber);
+        }
+
+        public void GoToWork()
+        {
+            Console.WriteLine("Doctor {0} {1} {2} goes to work ... ", surname, name, middleName);
+            patientNumber = patientNumber + 5;
+        }
+    }
+}
---
+++ ClassYulia/ClassYulia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassYulia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor();
+            doctor1.surname = "Ivanov";
+            doctor1.name = "Petr";
+            doctor1.middleName = "Alexeevich";
+            doctor1.profession = "terapevt";
+            doctor1.cabinetNumber = 15;
+            doctor1.patientNumber = 0;
+             
+            Doctor doctor2 = new Doctor();
+            doctor2.surname = "Limonov";
+            doctor2.name = "Viktor";
+            doctor2.middleName = "Sergeevich";
+            doctor2.profession = "oftolmolog";
+            doctor2.cabinetNumber = 7;
+            doctor2.patientNumber = 0;
+
+            doctor1.PrintBio();
+            doctor1.GoToWork();
+            doctor1.GoToWork();
+            doctor1.PrintBio();
+
+            doctor2.PrintBio();
+            doctor2.GoToWork();
+            doctor2.PrintBio();
+        }
+    }
+}
